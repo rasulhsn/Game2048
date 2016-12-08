f35582369b9c1a9aa13da14a86bc12e0a95d188e
Nishimura16 <resulhsn@gmail.com>
2016-12-08 21:35:57 +0400

    2048 Game Console variant
    2048 Game Algorithm

---
+++ Game_2048/Classes/Game_2048.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_2048.Classes
+{
+    public sealed class Game_2048
+    {
+        // Property Board...
+        private int[,] Matris_Board;
+        private List<bool> Action_Boards;
+
+
+        // Constructor..
+        public Game_2048()
+        {
+            this.Matris_Board = null;
+            this.Action_Boards = new List<bool>();
+        }
+        public Game_2048(int[,] Board)
+        {
+            if (Board.Length == 16)
+                this.Matris_Board = Board;
+            else
+                this.Matris_Board = null;
+
+            this.Action_Boards = new List<bool>();
+        }
+
+
+        public void DownKey()
+        {
+            DownNullControl();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 3; j >= 0; j--)
+                {
+                    if(this.Matris_Board[j,i] != 0)
+                    {
+                        if ((j - 1) == -1) break;
+                        else if (this.Matris_Board[j, i] == this.Matris_Board[j - 1, i])
+                        {
+                            this.Matris_Board[j, i] += this.Matris_Board[j - 1, i];
+                            this.Matris_Board[j - 1, i] = 0;
+                        }
+                    }
+                }
+            }
+
+            DownNullControl();
+        }
+        private void DownNullControl()
+        {
+            for (int i = 3; i >= 0; i--)
+            {
+                for (int j = 3; j >= 0; j--)
+                {
+                    if (this.Matris_Board[j, i] == 0)
+                    {
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            if (this.Matris_Board[j, i] == 0)
+                            {
+                                int next = this.Matris_Board[j, i];
+                                this.Matris_Board[j, i] = this.Matris_Board[k, i];
+                                this.Matris_Board[k, i] = next;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Using Left Collect Matris
+        public void LeftKey_v2()
+        {
+            LeftNullControl();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (this.Matris_Board[i, j] != 0)
+                    {
+                        if (j == 3) break;
+                        else if (this.Matris_Board[i, j] == this.Matris_Board[i, j + 1])
+                        {
+                            this.Matris_Board[i, j] += this.Matris_Board[i, j + 1];
+                            this.Matris_Board[i, j + 1] = 0;
+                        }
+                    }
+                }
+            }
+
+            LeftNullControl();
+        }
+        private void LeftKey()
+        {
+            LeftNullControl();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 3; j >= 0; j--)
+                {
+                    if (this.Matris_Board[i, j] != 0)
+                    {
+                        if (j == 0) break;
+                        else if (this.Matris_Board[i, j] == this.Matris_Board[i, j - 1])
+                        {
+                            this.Matris_Board[i, j - 1] += this.Matris_Board[i, j];
+                            this.Matris_Board[i, j] = 0;
+                        }
+                    }
+                }
+            }
+
+            LeftNullControl();
+        }
+        private void LeftNullControl()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (this.Matris_Board[i, j] == 0)
+                    {
+                        for (int k = j + 1; k < 4; k++)
+                        {
+                            if (this.Matris_Board[i, j] == 0)
+                            {
+                                int next = this.Matris_Board[i, k];
+                                this.Matris_Board[i, j] = next;
+                                this.Matris_Board[i, k] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // Using Right Collect Matris
+        public void RightKey_v2()
+        {
+            RightNullControl();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 3; j >- 0; j--)
+                {
+                    if (this.Matris_Board[i, j] != 0)
+                    {
+                        if (j == 0) break;
+                        else if (this.Matris_Board[i, j] == this.Matris_Board[i, j - 1])
+                        {
+                            this.Matris_Board[i, j] += this.Matris_Board[i, j - 1];
+                            this.Matris_Board[i, j-1] = 0;
+                        }
+                    }
+                }
+            }
+
+            RightNullControl();
+        }
+        private void RightKey()
+        {
+            RightNullControl();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (this.Matris_Board[i, j] != 0)
+                    {
+                        if (j == 3) break;
+                        else if (this.Matris_Board[i, j] == this.Matris_Board[i, j + 1])
+                        {
+                            this.Matris_Board[i, j + 1] += this.Matris_Board[i, j];
+                            this.Matris_Board[i, j] = 0;
+                        }
+                    }
+                }
+            }
+
+            RightNullControl();
+        }
+        private void RightNullControl()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 3; j >= 0; j--)
+                {
+                    if (this.Matris_Board[i, j] == 0)
+                    {
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            if (this.Matris_Board[i, j] == 0)
+                            {
+                                int next = this.Matris_Board[i, k];
+                                this.Matris_Board[i, j] = next;
+                                this.Matris_Board[i, k] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // Temporary..
+        public void Show_Matris()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(this.Matris_Board[i, j]);
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+    }
+}
---
+++ Game_2048/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game_2048.Classes;
+
+
+namespace Game_2048
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] MassiveMatris =
+            {
+                {4,0,2,2 },
+                {0,0,0,2 },
+                {4,0,8,0},
+                {0,0,0,0 }
+            };
+            int[,] MassiveMatris2 =
+            {
+                {4,0,0,2 },
+                {0,0,0,2 },
+                {4,0,8,0},
+                {0,0,0,0 }
+            };
+
+
+            Game_2048.Classes.Game_2048 GM = new Classes.Game_2048(MassiveMatris);
+            //Game_2048.Classes.Game_2048 GM2 = new Classes.Game_2048(MassiveMatris2);
+
+
+            GM.Show_Matris();
+            GM.DownKey();
+            Console.WriteLine();
+
+            GM.Show_Matris();
+            Console.WriteLine();
+            GM.LeftKey_v2();
+            GM.Show_Matris();
+
+            /* Testing All variants
+            GM.Show_Matris();
+            Console.WriteLine();
+            GM.LeftKey_v2();
+            Console.WriteLine();
+            GM.Show_Matris();
+            Console.WriteLine();
+            GM.LeftKey_v2();
+            Console.WriteLine();
+            GM.Show_Matris();
+
+            // GM Left P
+            GM.Show_Matris();
+            Console.WriteLine();
+            GM.LeftKey();
+            GM.Show_Matris();
+            Console.WriteLine();
+            // GM2 Left_v2
+            GM2.Show_Matris();
+            Console.WriteLine();
+            GM2.LeftKey_v2();
+            GM2.Show_Matris();
+            */
+
+            /*
+            GM.Show_Matris();
+            Console.WriteLine();
+
+            GM.LeftKey();
+            Console.WriteLine();
+
+            GM.Show_Matris();
+            Console.WriteLine();
+
+            GM.RightKey();
+            Console.WriteLine();
+
+            GM.Show_Matris();
+            */
+
+            //LeftNullControl(MassiveMatris);
+            //RightNullControl(MassiveMatris);
+
+            Console.ReadKey();
+        }
+       
+        public static void Show_Matris(int[,] Show)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(Show[i, j]);
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+        public static void DownNullControl(int[,] UpControl)
+        {
+            for (int i = 3; i >= 0; i--)
+            {
+                for (int j = 3; j >= 0; j--)
+                {
+                    if(UpControl[j,i] == 0)
+                    {
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            if(UpControl[j,i] == 0)
+                            {
+                                int next = UpControl[j, i];
+                                UpControl[j, i] = UpControl[k, i];
+                                UpControl[k, i] = next;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // First Logic Null Right
+        public static void RightNullControl(int[,] RightControl)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 3; j >= 0; j--)
+                {
+                    if (RightControl[i, j] == 0)
+                    {
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            if (RightControl[i, j] == 0)
+                            {
+                                int next = RightControl[i, k];
+                                RightControl[i, j] = next;
+                                RightControl[i, k] = 0;
+                            }
+                        }
+                    }
+                }
+            } 
+        }
+
+        // First Logic Null Left
+        public static void LeftNullControl(int[,] LeftControl)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (LeftControl[i, j] == 0)
+                    {
+                        for (int k = j + 1; k < 4; k++)
+                        {
+                            if (LeftControl[i, j] == 0)
+                            {
+                                int next = LeftControl[i, k];
+                                LeftControl[i, j] = next;
+                                LeftControl[i, k] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
